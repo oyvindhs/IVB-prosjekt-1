6a98db202aeb524f267923b9a14b5c2a0d48be3c
Øyvind Singsaas <76978734+oyvindhs@users.noreply.github.com>
2021-02-01 15:51:20 +0100

    Create Oppgave1.py

--- Oppgave1.py
+++ Oppgave1.py
@@ -9,11 +9,12 @@ from scipy.sparse import diags
 from matplotlib import pyplot as plt
 
 L = 100 #meter
-T = 1
+T = 180 #år
 N = 100 # N + 1 elementer
 dt = 1
 dz = L / N
 k_w = 6.97*10**(-5) #m/s
+c_eq = 2.1 #mol/m^3
 
 # Diffusjonsparameter
 def K(z):
@@ -40,27 +41,53 @@ K_merket = np.zeros(N-2)
 for m in range(1, N-2): #OBS! Riktig lengde på K_merket?
     K_merket[m] = K_ray[m+1] - K_ray[m-1] 
   
+
 #Konstruerer de tre diagonalene i L-matrisen (av hhv. lengde N, N + 1, N), ut fra oppgitt matrise
 L_upper = [-2*alpha*K_ray[0]]
 for i in range(1, N):
     L_upper.append((-alpha/4)*K_merket[i] - alpha*K_ray[i])
   
+
 L_mid = [1 + 2*alpha*K_ray[0] + gamma]
 for i in range(1, N + 1):
     L_mid.append((1 + 2*alpha*K_ray[i]))
   
+
 L_lower = [(alpha/4)*K_merket[1] - alpha*K_ray[1]]
 for i in range(2, N):
     L_lower.append((alpha/4)*K_merket[i] - alpha*K_ray[i])
 L_lower.append(-2*alpha*K_ray[N])
 
-#Konstruerer de tre diagonalene i R-matrisen (av hhv. lengde N, N + 1, N), ut fra oppgitt matrise (der en ser at de er helt like L, bare med motsatt fortegn)
-R_upper = - L_upper
+#Konstruerer de tre diagonalene i R-matrisen (av hhv. lengde N, N + 1, N), ut fra oppgitt matrise
+
+
+R_upper = [+2*alpha*K_ray[0]]
+for i in range(1, N):
+    R_upper.append((+alpha/4)*K_merket[i] + alpha*K_ray[i])
  
-R_mid = - L_mid
+
+R_mid = [1 - 2*alpha*K_ray[0] - gamma]
+for i in range(1, N + 1):
+    R_mid.append((1 - 2*alpha*K_ray[i]))
   
-R_lower = - L_lower
+
+R_lower = [(-alpha/4)*K_merket[1] + alpha*K_rat[1]]
+for i in range(2, N):
+    R_lower.append((-alpha/4)*K_merket[i] + alpha*K_rat[i])
+R_lower.append(2*alpha*K_ray[N])
 
 
 L = diags([L_upper, L_main, L_lower], offsets = [1, 0, -1])
-R = diags([R_upper, R_main, R_lower], offsets = [1, 0, -1])
+
+
+
+
+
+
+def simulate():
+    S_vec = np.zeros(N+1)
+    S[0] = 2*gamma*c_eq
+
+
+
+
