c03820eb3ff5f703354d4c93eecbf68d22835bef
Øyvind Singsaas <76978734+oyvindhs@users.noreply.github.com>
2021-02-01 14:38:12 +0100

    Update Oppgave1.py

--- Oppgave1.py
+++ Oppgave1.py
@@ -7,10 +7,10 @@ This is a temporary script file.
 import numpy as np
 
 L = 100 #meter
-T = 
+T = 1
 N = 100 # N + 1 elementer
-dt = 
-dN = L / N
+dt = 1
+dz = L / N
 
 # 
 def K(z):
@@ -22,6 +22,14 @@ def K(z):
   return K_0 + K_a*(z/z_a)*np.exp(-z/z_a) + (K_b*(L-z)/z_b) * np.exp(-(-L-z)/z_b)
 
 # Konstrueerer diagonal matrise
+
+alpha = dt / (2 * dz**2)
+K_array = np.zeros(N + 1)
+
+
+L_upper = np.zeros(N-1)
+L_main = np.zeros(N)
+L_lower = np.zeros(N-1)
 
 L = diags([L_upper, L_main, L_lower], offsets = [1, 0, -1])
 R = diags([R_upper, R_main, R_lower], offsets = [1, 0, -1])
