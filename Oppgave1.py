1c6de42e8d03dc7fa7eb6336fc53b647052d9c82
Øyvind Singsaas <76978734+oyvindhs@users.noreply.github.com>
2021-02-01 13:39:17 +0100

    Update Oppgave1.py

--- Oppgave1.py
+++ Oppgave1.py
@@ -7,5 +7,9 @@ This is a temporary script file.
 
 import numpy as np
 
-
-print(np.sin(0.5))
+def K(z):
+  K_0 = 
+  K_a = 
+  K_b = 
+  z_a = 0
+  return K_0 + K_a*(z/z_a)*np.exp(-z/z_a) + (K_b*(L_z)/z_b) * np.exp(-(-L-z)/z_b)
