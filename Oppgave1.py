231c8171556058540ce20d5ad5648bca974a2f11
Øyvind Singsaas <76978734+oyvindhs@users.noreply.github.com>
2021-02-01 13:40:30 +0100

    Update Oppgave1.py

--- Oppgave1.py
+++ Oppgave1.py
@@ -7,9 +7,11 @@ This is a temporary script file.
 
 import numpy as np
 
+L = 1
+
 def K(z):
-  K_0 = 
-  K_a = 
-  K_b = 
+  K_0 = 1
+  K_a = 1
+  K_b = 1
   z_a = 0
   return K_0 + K_a*(z/z_a)*np.exp(-z/z_a) + (K_b*(L_z)/z_b) * np.exp(-(-L-z)/z_b)
